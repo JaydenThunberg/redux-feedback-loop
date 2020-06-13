fa74fa0dad9cbcd8547f6d4baff379381b187cce
JaydenThunberg <thunberg.jayden@gmail.com>
2020-06-13 17:45:23 -0500

    database connected

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool.js');
+
+// POST route to add a new feedback with feeling, understanding, support, & comments.
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const queryString = `INSERT INTO feedback (feeling, understanding, support, comments) VALUES ($1, $2, $3, $4)`;
+    const values= [req.body.feeling, req.body.understanding, req.body.support, req.body.comments]
+    pool.query(queryString, values)
+      .then((result) => {
+        console.log(`added feedback to DB`, newFeedback);
+        res.sendStatus(201);
+      })
+      .catch((error) => {
+        console.log(`error with DB ${queryString}`, error);
+        res.sendStatus(500); // server response if error
+      })
+  })// end POST 
+
+  router.get('/admin', (req, res) => {
+    const sqlString = `SELECT * FROM feedback ORDER BY id ASC;`;
+    pool.query(sqlString)
+      .then((result) => {
+        console.log(`feedback from GET:`, result.rows);
+        res.send(result.rows);
+      })
+      .catch((error) => {
+        console.log(`error with database ${sqlString}`, error);
+        res.sendStatus(500); 
+      })
+  })// end GET
+
+  module.exports = router;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const formReducer = () => {
+    console.log('in formReducer')
+}//end formReducer
+
+const storeInstance = createStore( formReducer);
+ReactDOM.render(
+<Provider store={storeInstance}>
+    <App />
+</Provider>,
+document.getElementById('root')
+);
 registerServiceWorker();
