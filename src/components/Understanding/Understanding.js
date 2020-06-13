fdfb9f81aac95ae342968ce11b50a71576be7061
JaydenThunberg <thunberg.jayden@gmail.com>
2020-06-13 18:01:00 -0500

    understanding select menu working

--- src/components/Understanding/Understanding.js
+++ src/components/Understanding/Understanding.js
@@ -1,10 +1,40 @@
 import React, {Component} from 'react';
+import { Button, FormControl, MenuItem, Select } from '@material-ui/core/';
 
 class Understanding extends Component{
+    state = {
+        understanding: ''
+    } //end state
+
+    submitUnderstanding = () => {
+        console.log('submitUnderstanding:', this.state.understanding);
+        this.props.history.push('/understanding')
+    }//end handle click
+
+    handleUnderstanding = (event) => {
+        this.setState({
+            understanding: event.target.value,
+        })
+        console.log('in handleUnderstanding');
+    }// end handleUnderstanding
     render(){
         return(
             <div>
-                <h1>Understanding</h1>
+                <h1>How well are you understanding the content?</h1>
+                <br/>
+                <FormControl>
+                <Select value={this.state.understanding} onChange={this.handleUnderstanding}>
+                <MenuItem value=""><em>Understanding?</em></MenuItem>
+                    <MenuItem value="5">Excellent</MenuItem>
+                    <MenuItem value="4">Very Good</MenuItem>
+                    <MenuItem value="3">Good</MenuItem>
+                    <MenuItem value="2">Fair</MenuItem>
+                    <MenuItem value="1">Poor</MenuItem>
+                </Select>
+                </FormControl>
+                <br />
+                <br />
+                <Button size="small" variant="outlined" color="secondary" onClick={this.submitFeeling}>Next</Button>
             </div>
         )//end return
     }//end render
