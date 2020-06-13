a4b7a9ac960f2f5643f7b9c5cdee984555b3f6f3
JaydenThunberg <thunberg.jayden@gmail.com>
2020-06-13 16:26:37 -0500

    feeling captures menuItem

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
 import Feeling from '../Feeling/Feeling';
 import Understanding from '../Understanding/Understanding';
--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,13 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Button, FormControl, MenuItem, Select } from '@material-ui/core/';
 
-class Feeling extends Component{
-    render(){
-        return(
+class Feeling extends Component {
+    state = {
+        feeling: ''
+    } //end state
+
+    submitFeeling = () => {
+        console.log('submitFeeling:', this.state.feeling);
+        this.props.history.push('/understanding')
+    }//end handle click
+
+    handleFeeling = (event) => {
+        this.setState({
+            feeling: event.target.value,
+        })
+        console.log('in handleFeeling feeling');
+    }// end handleFeeling
+
+
+
+    render() {
+        return (
             <div>
-                <h1>Feeling</h1>
+                <h1>How are you feeling today?</h1>
+                <br/>
+                <FormControl>
+                <Select value={this.state.feeling} onChange={this.handleFeeling}>
+                <MenuItem value=""><em>Feeling?</em></MenuItem>
+                    <MenuItem value="5">Excellent</MenuItem>
+                    <MenuItem value="4">Very Good</MenuItem>
+                    <MenuItem value="3">Good</MenuItem>
+                    <MenuItem value="2">Fair</MenuItem>
+                    <MenuItem value="1">Poor</MenuItem>
+                </Select>
+                </FormControl>
+                <br />
+                <br />
+                <Button size="small" variant="outlined" color="secondary" onClick={this.submitFeeling}>Next</Button>
             </div>
-        )//end return
-    }//end render
-}//end class
+        ) //end return
+    } //end render
+} //end class
 
 export default Feeling;
