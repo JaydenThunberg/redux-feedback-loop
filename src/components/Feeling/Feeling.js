84edd20e45aaef4498579475b716aa102b0b0095
JaydenThunberg <thunberg.jayden@gmail.com>
2020-06-13 18:46:13 -0500

    feeling sent to reducer

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux';
 import { Button, FormControl, MenuItem, Select } from '@material-ui/core/';
 
 class Feeling extends Component {
@@ -8,7 +9,17 @@ class Feeling extends Component {
 
     submitFeeling = () => {
         console.log('submitFeeling:', this.state.feeling);
+        if (this.state.feeling === '') {
+        alert('Please select how you are feeling, then press Next. Thank you!');
+        } else if (this.state.feeling > 0) {
+        this.props.dispatch({
+        type: 'NEW_FEELING',
+        payload: this.state.feeling,
+        })
         this.props.history.push('/understanding')
+        }
+
+        // this.props.history.push('/understanding')
     }//end submitFeeling
 
     handleFeeling = (event) => {
@@ -24,16 +35,16 @@ class Feeling extends Component {
         return (
             <div>
                 <h1>How are you feeling today?</h1>
-                <br/>
+                <br />
                 <FormControl variant="outlined" color="secondary">
-                <Select value={this.state.feeling} onChange={this.handleFeeling}>
-                <MenuItem value=""><em>Feeling?</em></MenuItem>
-                    <MenuItem value="5">Excellent</MenuItem>
-                    <MenuItem value="4">Very Good</MenuItem>
-                    <MenuItem value="3">Good</MenuItem>
-                    <MenuItem value="2">Fair</MenuItem>
-                    <MenuItem value="1">Poor</MenuItem>
-                </Select>
+                    <Select value={this.state.feeling} onChange={this.handleFeeling}>
+                        <MenuItem value=""><em>Feeling?</em></MenuItem>
+                        <MenuItem value="5">Excellent</MenuItem>
+                        <MenuItem value="4">Very Good</MenuItem>
+                        <MenuItem value="3">Good</MenuItem>
+                        <MenuItem value="2">Fair</MenuItem>
+                        <MenuItem value="1">Poor</MenuItem>
+                    </Select>
                 </FormControl>
                 <br />
                 <br />
@@ -43,4 +54,4 @@ class Feeling extends Component {
     } //end render
 } //end class
 
-export default Feeling;
+export default connect()(Feeling);
