33674ed6c4812692536f9f901d82f0efc757e345
JaydenThunberg <thunberg.jayden@gmail.com>
2020-06-13 18:38:53 -0500

    added formReducer catch information

--- src/index.js
+++ src/index.js
@@ -6,8 +6,21 @@ import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-const formReducer = () => {
-    console.log('in formReducer')
+const formReducer = (state = 0, action) => {
+    console.log('in formReducer', 'state:', state, 'action.type:', action.type);
+  if (action.type === 'NEW_FEELING') {
+    return { ...state, feeling: action.payload }
+  }
+  else if (action.type === 'NEW_UNDERSTANDING') {
+    return { ...state, understanding: action.payload }
+  }
+  else if (action.type === 'NEW_SUPPORT') {
+    return { ...state, support: action.payload }
+  }
+  else if (action.type === 'NEW_COMMENT') {
+    return { ...state, comments: action.payload }
+  }
+  return state;
 }//end formReducer
 
 const storeInstance = createStore( formReducer);
