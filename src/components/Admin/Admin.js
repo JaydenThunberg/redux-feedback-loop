9441a79a7e3ac67971cc824d7813cc71164f53dc
JaydenThunberg <thunberg.jayden@gmail.com>
2020-06-13 22:27:49 -0500

    feedback coming to /#/admin

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -4,23 +4,45 @@ import { connect } from 'react-redux';
 import { Button } from '@material-ui/core';
 
 class Admin extends Component{
+    state = {
+        feedback: []
+      }
+
+      componentDidMount(){
+          console.log('componentDidMount')
+          this.getFeedbacK();
+      }//end component
     
+      getFeedbacK() {
+        console.log('in GET feedback')
+        axios.get('/feedback/admin')
+          .then((response) => {
+            console.log('GET feedback:', response.data);
+            this.setState({
+              feedback: response.data
+            })
+          })
+          .catch((error) => {
+            console.log('error with GET:', error);
+            alert('error getting admin feedback. Sorry, try again later.')
+          })
+      }//end getFeedback
 
     goHome = () =>{
         console.log('in handleClick admin');
         this.props.history.push('/')
-    } 
+    }//end goHome
 
     render(){
         return(
             <div>
             <h2>Welcome to Admin</h2>
-
+            {JSON.stringify(this.state.feedback)} // map through this to get on table
             <Button size="small"variant="contained" color="secondary" onClick={this.goHome}>Leave Admin</Button>
             </div>
-        )//return
-    }//render
-}//class
+        )//end return
+    }//end render
+}//end class
 
 const putReduxStateOnProps = (reduxState) => ({ reduxState });
 export default connect(putReduxStateOnProps)(Admin);
